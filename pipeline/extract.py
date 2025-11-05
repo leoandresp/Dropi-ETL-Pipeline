e710be1e400ba0dee76ed70469a789aaf6006ce6
Leonardo Polanco <leoandres52@gmail.com>
2025-11-05 00:27:52 -0400

    Se activó el headless option a el scripping de selenium

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -39,7 +39,7 @@ def extract_data():
     print(f"Inicia la extracción de Datos desde DROPI")
     
     #Configuramos la conexión con el navegador
-    driver = sc.WebDriverManager().get_driver()
+    driver = sc.WebDriverManager().get_driver(True)
     driver.get(DROPI_WEB)
     
     #Nos logueamos en la Página de Dropi
