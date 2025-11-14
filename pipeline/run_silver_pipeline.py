c1de03a4de55fa6e46b41f495458cce040b1694b
Leonardo Polanco <leoandres52@gmail.com>
2025-11-14 00:17:43 -0400

    Adición de logging pendiente en pipeline capa silver

--- pipeline/run_silver_pipeline.py
+++ pipeline/run_silver_pipeline.py
@@ -4,6 +4,7 @@ from pipeline.transform import *
 from config import *
 
 
+logging.info("Extrayendo datos de la ingesta mas reciente.")
 raw_data = silver_data_extract()
 clean_data = clean_raw_data(raw_data)
 transformed_data = silver_data_transform(clean_data)
